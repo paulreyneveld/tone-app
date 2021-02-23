5f7cb966af96ddc6f692d2bae8d372612f7d1717
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-02-22 19:21:20 -0800

    Added a button to play a note

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import * as Tone from 'tone';
 
-function App() {
+
+const playC4 = () => {
+  //create a synth and connect it to the main output (your speakers)
+  const synth = new Tone.Synth().toDestination();
+
+  //play a middle 'C' for the duration of an 8th note
+  synth.triggerAttackRelease("C4", "8n");
+}
+
+const App = () => {
   return (
     <div>
-
+      <button onclick={playC4}>Play C4</button>
     </div>
   );
 }
