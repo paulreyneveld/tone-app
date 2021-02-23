a9b27e4ba84ff272302042cd9fc7cf455750761b
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-02-22 20:03:04 -0800

    Final commit

--- src/App.js
+++ src/App.js
@@ -12,13 +12,31 @@ import {
   playA4,
   playBb4,
   playB4,
-  playC5      
+  playC5,
+  playNote      
 } from './tone.fn'
+
+window.addEventListener("keydown", playNote);
 
 const App = () => {
   return (
-    <div>
-      <button onClick={playC4}>Play C4</button>
+    <div className="pianoPage">
+      <h1>Piano</h1>
+      <div className="piano">
+        <div className="white-key" onClick={playC4}>A</div>
+        <div className="black-key" onClick={playDb4}>W</div>
+        <div className="white-key" onClick={playD4}>S</div>
+        <div className="black-key" onClick={playEb4}>E</div>
+        <div className="white-key" onClick={playE4}>D</div>
+        <div className="white-key" onClick={playF4}>F</div>
+        <div className="black-key" onClick={playGb4}>T</div>
+        <div className="white-key" onClick={playG4}>G</div>
+        <div className="black-key" onClick={playAb4}>Y</div>
+        <div className="white-key" onClick={playA4}>H</div>
+        <div className="black-key" onClick={playBb4}>U</div>
+        <div className="white-key" onClick={playB4}>J</div>
+        <div className="white-key" onClick={playC5}>K</div>
+      </div>
     </div>
   );
 }
--- src/tone.fn.js
+++ src/tone.fn.js
@@ -64,3 +64,45 @@ export const playC5 = () => {
     const synth = new Tone.Synth().toDestination();
     synth.triggerAttackRelease("C5", "8n");
 }
+
+export function playNote(event) {
+    if (event.keyCode === 65) {
+      playC4();
+    }
+    if (event.keyCode === 87) {
+      playDb4();
+    }
+    if (event.keyCode === 83) {
+      playD4();
+    }
+    if (event.keyCode === 69) {
+      playEb4();
+    }
+    if (event.keyCode === 68) {
+      playE4();
+    }
+    if (event.keyCode === 70) {
+      playF4();
+    }
+    if (event.keyCode === 84) {
+      playGb4();
+    }
+    if (event.keyCode === 71) {
+      playG4();
+    }
+    if (event.keyCode === 89) {
+      playAb4();
+    }
+    if (event.keyCode === 72) {
+      playA4();
+    }
+    if (event.keyCode === 85) {
+      playBb4();
+    }
+    if (event.keyCode === 74) {
+      playB4();
+    }
+    if (event.keyCode === 75) {
+      playC5();
+    }
+  }
